fffae99acd18a58155133327bfcfddad78368066
naerhy <naerhy@protonmail.com>
2024-09-23 00:19:54 +0200

    Remove remaining comment

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,6 @@ import WatchesModel from "./models/WatchesModel";
 import WatchesView from "./views/WatchesView";
 import WatchesController from "./controllers/WatchesController";
 
-// TODO: check unused methods and properties on each class -> delete them
-
 const watchesModel = new WatchesModel();
 const watchesView = new WatchesView();
 new WatchesController(watchesModel, watchesView).start();
